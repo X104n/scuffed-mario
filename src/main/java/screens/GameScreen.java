6867494cc5e3f2a12f9f434793c63262562389e1
Magne.Stenseng <magne.stenseng@student.uib.no>
2022-02-20 20:23:02 +0000

    Replace GameScreen.java, adding code for the (potential) running animation. Couldn't test it myself on my hunk-of-junk machine, but should work.

--- src/main/java/screens/GameScreen.java
+++ src/main/java/screens/GameScreen.java
@@ -5,7 +5,9 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -20,6 +22,14 @@ public class GameScreen implements Screen {
 
     Texture player;
     Texture backGroundImage;
+
+    //variables to make the running animations
+    Animation<TextureRegion> rightRun;
+    Animation<TextureRegion> leftRun;
+    Texture runSheet;
+    SpriteBatch sprite;
+    float spriteTime;
+
 
     float playerX = 0;
     float playerY = 0;
@@ -62,18 +72,22 @@ public class GameScreen implements Screen {
 
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
             System.out.println("w, was pressed"); // just for debugging
+            rightRunning();
             playerY += Gdx.graphics.getDeltaTime() * Speed;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
             System.out.println("s, was pressed");
+            leftRunning();
             playerY -= Gdx.graphics.getDeltaTime() * Speed;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             System.out.println("a, was pressed");
+            leftRunning();
             playerX -= Gdx.graphics.getDeltaTime() * Speed;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             System.out.println("d, was pressed");
+            rightRunning();
             playerX += Gdx.graphics.getDeltaTime() * Speed;
         }
 
@@ -89,6 +103,60 @@ public class GameScreen implements Screen {
         batch.end();
 
     }
+
+    //class that makes the running animations
+    public void createSprite() {
+
+        //adding the sprite sheet
+        runSheet = new Texture((Gdx.files.internal("assets/MarioRunSheet")));
+
+        //splitting the sheet based on the number of columns and rows,
+        // so we gain access to each individual sprite
+        TextureRegion[][] temp = TextureRegion.split
+                (runSheet, runSheet.getWidth()/5, runSheet.getHeight()/4);
+
+        TextureRegion[] runningRight = new TextureRegion[5];
+        TextureRegion[] runningLeft = new TextureRegion[5];
+
+        //adding the SPECIFIC sprites to make the character run to the right.
+        runningRight[0] = temp[3][0];
+        runningRight[1] = temp[4][0];
+        runningRight[2] = temp[0][1];
+        runningRight[3] = temp[1][1];
+        runningRight[4] = temp[2][1];
+        runningRight[5] = temp[3][1];
+
+        //and again, but to make the character run to the right.
+        runningLeft[0] = temp[1][2];
+        runningLeft[1] = temp[2][2];
+        runningLeft[2] = temp[3][2];
+        runningLeft[3] = temp[4][2];
+        runningLeft[4] = temp[0][3];
+        runningLeft[5] = temp[1][3];
+
+        rightRun = new Animation<TextureRegion>(0.025f, runningRight);
+        leftRun = new Animation<TextureRegion>(0.025f, runningLeft);
+
+        sprite = new SpriteBatch();
+        spriteTime = 0f;
+    }
+
+    //specific class to make the player run to the right
+    public void rightRunning(){
+        TextureRegion rightPlayer = rightRun.getKeyFrame(spriteTime, true);
+        sprite.begin();
+        sprite.draw(rightPlayer, playerX, playerY);
+        sprite.end();
+    }
+
+    //specific class to make the player run to the left
+    public void leftRunning(){
+        TextureRegion leftPlayer = leftRun.getKeyFrame(spriteTime, true);
+        sprite.begin();
+        sprite.draw(leftPlayer, playerX, playerY);
+        sprite.end();
+    }
+
 
     @Override
     public void resize(int i, int i1) {
@@ -114,5 +182,7 @@ public class GameScreen implements Screen {
     public void dispose() {
         backGroundImage.dispose();
         player.dispose();
+        sprite.dispose();
+        runSheet.dispose();
     }
 }
