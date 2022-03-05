ab684cec3995821d157f437ee4dffaa038caa778
Magne.Stenseng <magne.stenseng@student.uib.no>
2022-03-05 20:56:51 +0000

    Replace GameScreen.java with a "completed" version. However, it does not work yet. A meeting on 08/03 is wished for.

--- src/main/java/screens/GameScreen.java
+++ src/main/java/screens/GameScreen.java
@@ -12,28 +12,27 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GameScreen implements Screen {
-
     final ScuffedMario game;
-
     OrthographicCamera camera;
-
     Stage stage;
     SpriteBatch batch;
-
-    Texture player;
+    TextureRegion player;
     Texture backGroundImage;
-
     float playerX = 0;
     float playerY = 0;
     float Speed = 50.0f;
 
-    int SCENE_HEIGHT = 480;
-    int SCENE_WIDTH = 800;
+    public static Texture mario = new Texture("assets/MarioRunSheet.png");
+    static Animation mario_run;
+    static TextureRegion[] run_frame;
+    static TextureRegion current_frame;
+    static float state_time;
+
+    boolean running;
+
 
     public GameScreen(final ScuffedMario game) {
         this.game = game;
-
-
         // load the test image
         //marioImage = new Texture(Gdx.files.internal("assets/notFinalScuffedMario.png"));
         //backGroundImage = new Texture(Gdx.files.internal("assets/testBackground.png"));
@@ -42,13 +41,35 @@ public class GameScreen implements Screen {
         camera = new OrthographicCamera();
 
         // Note that we make the camera a fixed size here so if we want to show more at a time we need to upscale it here
-        camera.setToOrtho(false, SCENE_WIDTH, SCENE_HEIGHT);
+        camera.setToOrtho(false, 800, 480);
+    }
+
+    public void create() {
+        mario = new Texture(Gdx.files.internal("bounce_sprite.png"));
+        TextureRegion[][] tmp = TextureRegion.split(mario, mario.getWidth()/13, mario.getHeight());
+        run_frame = new TextureRegion[3];
+        int index = 0;
+        for(int y=1; y<4; y++) {
+            for(int x=0; x<1; x++) {
+                run_frame[index++] = tmp[y][x];
+            }
+        }
+        mario_run = new Animation(1f/4f, run_frame);
+        state_time = 0f;
+
+        // set this variable true when you want the animation to run once
+        running = true;
+
     }
 
     @Override
     public void show() {
-        player = new Texture("assets/notFinalScuffedMario.png");
+        player = new TextureRegion(mario, 0, 0, 16, 16);
+
+        //player = new TextureRegion(texture, 0,0,16,16);
+                //new Texture("assets/notFinalScuffedMario.png");
         backGroundImage = new Texture("assets/testBackground.png");
+
 
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
@@ -62,28 +83,39 @@ public class GameScreen implements Screen {
 
         //Character part
         batch.begin();
-        batch.draw(backGroundImage, 0, 0, SCENE_WIDTH, SCENE_HEIGHT);
+        batch.draw(backGroundImage, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.draw(player, playerX, playerY, 64, 64);
 
+        state_time += Gdx.graphics.getDeltaTime() * Speed;
+        current_frame = (TextureRegion) mario_run.getKeyFrame(state_time, true);
+
+        //MarioRun runRight = new MarioRun(new TextureRegion(texture), 3, 0.5f, 1, 3);
+
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            System.out.println("w, was pressed"); // just for debugging
+            //System.out.println("w, was pressed"); // just for debugging
             playerY += Gdx.graphics.getDeltaTime() * Speed;
+
         }
         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            System.out.println("s, was pressed");
+            //System.out.println("s, was pressed");
             playerY -= Gdx.graphics.getDeltaTime() * Speed;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            System.out.println("a, was pressed");
+            //System.out.println("a, was pressed");
             playerX -= Gdx.graphics.getDeltaTime() * Speed;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            System.out.println("d, was pressed");
+            //System.out.println("d, was pressed");
             playerX += Gdx.graphics.getDeltaTime() * Speed;
+            batch.draw(current_frame, playerX, playerY, 64, 64);
+            if(mario_run.isAnimationFinished(state_time)) {
+                state_time = Gdx.graphics.getDeltaTime() * Speed;
+                running = false;
+            }
         }
 
-
         camera.update();
+
 
         // tell the SpriteBatch to render in the
         // coordinate system specified by the camera.
@@ -118,6 +150,10 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         backGroundImage.dispose();
-        player.dispose();
+        //player.dispose();
+        /*
+        * Is not used since player is just a region of texture.
+        * texture is disposed instead.
+        * */
     }
 }
