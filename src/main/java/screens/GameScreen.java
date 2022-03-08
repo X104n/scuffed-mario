71c710c4b44e53d3ad9736dbbc653ec40655fbcb
Magne.Stenseng <magne.stenseng@student.uib.no>
2022-03-08 14:10:45 +0000

    Updated GameScreen.java to include Hud.java.

--- src/main/java/screens/GameScreen.java
+++ src/main/java/screens/GameScreen.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -22,13 +21,9 @@ public class GameScreen implements Screen {
     float playerY = 0;
     float Speed = 50.0f;
 
-    public static Texture mario = new Texture("assets/MarioRunSheet.png");
-    static Animation mario_run;
-    static TextureRegion[] run_frame;
-    static TextureRegion current_frame;
-    static float state_time;
+    final Hud hud;
 
-    boolean running;
+    public static Texture mario = new Texture("assets/MarioRunSheet.png");
 
 
     public GameScreen(final ScuffedMario game) {
@@ -40,34 +35,19 @@ public class GameScreen implements Screen {
         // Creates a new camera for the game screen
         camera = new OrthographicCamera();
 
-        // Note that we make the camera a fixed size here so if we want to show more at a time we need to upscale it here
+        // Note that we make the camera a fixed size here
+        // so if we want to show more at a time we need to upscale it here
         camera.setToOrtho(false, 800, 480);
+
+        //creating the HUD.
+        hud = new Hud(game.batch);
     }
 
-    public void create() {
-        mario = new Texture(Gdx.files.internal("bounce_sprite.png"));
-        TextureRegion[][] tmp = TextureRegion.split(mario, mario.getWidth()/13, mario.getHeight());
-        run_frame = new TextureRegion[3];
-        int index = 0;
-        for(int y=1; y<4; y++) {
-            for(int x=0; x<1; x++) {
-                run_frame[index++] = tmp[y][x];
-            }
-        }
-        mario_run = new Animation(1f/4f, run_frame);
-        state_time = 0f;
-
-        // set this variable true when you want the animation to run once
-        running = true;
-
-    }
 
     @Override
     public void show() {
         player = new TextureRegion(mario, 0, 0, 16, 16);
 
-        //player = new TextureRegion(texture, 0,0,16,16);
-                //new Texture("assets/notFinalScuffedMario.png");
         backGroundImage = new Texture("assets/testBackground.png");
 
 
@@ -78,6 +58,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float v) {
+
         //screen part:
         ScreenUtils.clear(0, 0, 0, 1);
 
@@ -85,11 +66,6 @@ public class GameScreen implements Screen {
         batch.begin();
         batch.draw(backGroundImage, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.draw(player, playerX, playerY, 64, 64);
-
-        state_time += Gdx.graphics.getDeltaTime() * Speed;
-        current_frame = (TextureRegion) mario_run.getKeyFrame(state_time, true);
-
-        //MarioRun runRight = new MarioRun(new TextureRegion(texture), 3, 0.5f, 1, 3);
 
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
             //System.out.println("w, was pressed"); // just for debugging
@@ -107,11 +83,6 @@ public class GameScreen implements Screen {
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             //System.out.println("d, was pressed");
             playerX += Gdx.graphics.getDeltaTime() * Speed;
-            batch.draw(current_frame, playerX, playerY, 64, 64);
-            if(mario_run.isAnimationFinished(state_time)) {
-                state_time = Gdx.graphics.getDeltaTime() * Speed;
-                running = false;
-            }
         }
 
         camera.update();
@@ -121,8 +92,9 @@ public class GameScreen implements Screen {
         // coordinate system specified by the camera.
         batch.setProjectionMatrix(camera.combined);
 
+        //drawing the hud (will be updated along with the rest of the code)
+        hud.stage.draw();
 
-        batch.setProjectionMatrix(camera.combined);
         batch.end();
 
     }
