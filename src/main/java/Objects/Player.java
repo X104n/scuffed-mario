2d1e412d44b11a9091e3eb6ffd9d1a9de7ea7f3b
Magne.Stenseng <magne.stenseng@student.uib.no>
2022-03-25 18:50:14 +0000

    Upload New File

---
+++ src/main/java/Objects/Player.java
@@ -0,0 +1,62 @@
+package Objects;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+
+import static Tools.Constants.PPM;
+
+public class Player extends Entity {
+
+    private int jumpCounter;
+    public boolean moved = false;
+
+    public Player(float width, float height, Body body) {
+        super(width, height, body);
+        this.speed = 10f;
+        this.jumpCounter = 0;
+    }
+
+    @Override
+    public void update() {
+        x = body.getPosition().x * PPM;
+        y = body.getPosition().y * PPM;
+
+        checkUserInput();
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+
+    private void checkUserInput(){
+        velX = 0;
+        if(Gdx.input.isKeyPressed(Input.Keys.D)){
+            velX = 1;
+            moved = true;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.A)){
+            velX = -1;
+            moved = true;
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && jumpCounter < 2){
+            float force = body.getMass() * 18;
+            body.setLinearVelocity(body.getLinearVelocity().x, 0);
+            body.applyLinearImpulse(new Vector2(0, force), body.getPosition(), true);
+            jumpCounter++;
+            moved = true;
+        }
+        // reset jump counter
+        if(body.getLinearVelocity().y == 0){
+            jumpCounter = 0;
+        }
+        body.setLinearVelocity(velX * speed, body.getLinearVelocity().y < 25 ? body.getLinearVelocity().y : 25);
+    }
+
+    public boolean isMoved() {
+        return moved;
+    }
+}
