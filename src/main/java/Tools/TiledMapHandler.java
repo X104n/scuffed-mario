5af93e82404ae1774556a283fc27f13852ae4bc8
Magne.Stenseng <magne.stenseng@student.uib.no>
2022-03-25 18:53:57 +0000

    Upload New File

---
+++ src/main/java/Tools/TiledMapHandler.java
@@ -0,0 +1,94 @@
+package Tools;
+
+import Objects.Player;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
+import screens.GameScreen;
+
+import static Tools.Constants.PPM;
+
+public class TiledMapHandler {
+    private TiledMap tiledMap;
+    private GameScreen gameScreen;
+    private boolean creation = false;
+    private boolean createPlayer = false;
+
+    public TiledMapHandler(GameScreen gameScreen){
+        this.gameScreen = gameScreen;
+    }
+
+    public OrthoCachedTiledMapRenderer setupMap() {
+        tiledMap = new TmxMapLoader().load("assets/Map/level0.tmx");
+        parseMapObjects(tiledMap.getLayers().get("objects").getObjects());
+        return new OrthoCachedTiledMapRenderer(tiledMap);
+    }
+
+    private void parseMapObjects(MapObjects mapObjects) {
+        for (MapObject mapObject : mapObjects) {
+            if (mapObject instanceof PolygonMapObject) {
+                createStaticBody((PolygonMapObject) mapObject);
+                if(creation == false){
+                    creation = true;
+                }
+            }
+            if (mapObject instanceof RectangleMapObject){
+                Rectangle rectangle = ((RectangleMapObject) mapObject).getRectangle();
+                String rectangleName = mapObject.getName();
+                if(rectangleName.equals("player")){
+                    Body body = EntityBuilder.createBody(
+                            rectangle.getX() + rectangle.getWidth() /2,
+                            rectangle.getY() + rectangle.getHeight() / 2,
+                            rectangle.getWidth(),
+                            rectangle.getHeight(),
+                            false,
+                            gameScreen.getWorld()
+                    );
+                    createPlayer = true;
+                    System.out.println("123");
+                    gameScreen.setPlayer(new Player(rectangle.getWidth(), rectangle.getHeight(), body));
+                    System.out.println("123");
+                }
+            }
+        }
+    }
+
+    private void createStaticBody(PolygonMapObject polygonMapObject) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        Body body = gameScreen.getWorld().createBody(bodyDef);
+        Shape shape = createPolygonShape(polygonMapObject);
+        body.createFixture(shape, 1000);
+        shape.dispose();
+    }
+
+    private Shape createPolygonShape(PolygonMapObject polygonMapObject) {
+        float[] vertices = polygonMapObject.getPolygon().getTransformedVertices();
+        Vector2[] worldVertices = new Vector2[vertices.length / 2];
+        for (int i = 0; i < vertices.length / 2; i++) {
+            Vector2 current = new Vector2(vertices[i * 2] / PPM, vertices[i * 2 + 1] / PPM);
+            worldVertices[i] = current;
+        }
+        PolygonShape shape = new PolygonShape();
+        shape.set(worldVertices);
+        return shape;
+    }
+
+    public boolean isCreated() {
+        return creation;
+    }
+
+    public boolean isCreatePlayer() {
+        return createPlayer;
+    }
+}
