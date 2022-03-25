e4fbb6e8d67d80114574ed2040a1a1eaf8746e51
Magne.Stenseng <magne.stenseng@student.uib.no>
2022-03-25 18:52:06 +0000

    Replace ScuffedMario.java

--- src/main/java/screens/ScuffedMario.java
+++ src/main/java/screens/ScuffedMario.java
@@ -1,30 +1,32 @@
 package screens;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import screens.GameScreen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class ScuffedMario extends Game {
-    public static final int PPM = 1;
 
-    // We should only use one sprite batch, so we make this one public.
-    public SpriteBatch batch;
+    public static ScuffedMario INSTANCE;
+    private int widthScreen, heightScreen;
+    private OrthographicCamera orthographicCamera;
+    private GameScreen game;
 
-    // Can be used to create front in the future.
-    public BitmapFont font;
+    public ScuffedMario(){
+        INSTANCE = this;
+    }
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-
-        this.setScreen(new GameScreen(this));
+        this.widthScreen = Gdx.graphics.getWidth();
+        this.heightScreen = Gdx.graphics.getHeight();
+        this.orthographicCamera = new OrthographicCamera();
+        this.orthographicCamera.setToOrtho(false, widthScreen, heightScreen);
+        game = new GameScreen(this.orthographicCamera);
+        setScreen(game);
     }
 
-    public void render(){super.render();}
-
-    public void dispose(){
-        batch.dispose();
+    public GameScreen getGame() {
+        return this.game;
     }
 }
 
