7267c545fac3ad358d374cf2fffdd1f5a3731122
Sander Kavli <Sander.Kavli@uib.no>
2022-04-25 17:52:29 +0200

    Added movement logic to Boss : Now able to jump

--- src/main/java/Objects/Boss.java
+++ src/main/java/Objects/Boss.java
@@ -8,18 +8,26 @@ import java.awt.*;
 
 public class Boss extends Entity{
 
-
     GameScreen screen;
+    long lastjump;
+    double JUMPCD = 5.0;
     public Boss(float width, float height, Body body, GameScreen screen) {
         super(width, height, body);
-
+        velY = 0;
         this.screen = screen;
         super.type = ObjectType.BOSS;
+        lastjump = System.currentTimeMillis();
     }
 
     @Override
     public void update() {
-
+        long currentTime = System.currentTimeMillis();
+        if(currentTime - lastjump > JUMPCD) {
+            lastjump = currentTime;
+            velY = 5;
+        }
+        velY -= 2;
+        body.setLinearVelocity(body.getLinearVelocity().x, velY < 25 ? body.getLinearVelocity().y : 25);
     }
 
     @Override
