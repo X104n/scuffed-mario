761167a207cf559e3fa1b377647e3c3879bee0e9
Ben Mac Pro <belminhus@gmail.com>
2022-04-25 15:40:06 +0200

    added font, buttons now work but button-texture is broken

--- src/main/java/screens/MainMenu.java
+++ src/main/java/screens/MainMenu.java
@@ -1,22 +1,15 @@
 package screens;
 
-import Tools.Assets;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.assets.AssetDescriptor;
-import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.SkinLoader;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -25,68 +18,97 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import desktop.DesktopLauncher;
 
-import static Tools.Assets.SKIN;
-
 public class MainMenu implements Screen {
     Viewport viewport;
     Stage stage;
     OrthographicCamera camera;
     SpriteBatch batch;
     ScuffedMario mario;
-    Table table;
     Skin skin;
-    AssetManager assetManager;
+    TextureAtlas textureAtlas;
 
     Music backgroundMusic;
     Texture background;
 
-    public MainMenu(AssetManager assetManager) {
-        this.assetManager = assetManager;
-        skin = assetManager.get(SKIN);
-    }
 
     public MainMenu(ScuffedMario mario, OrthographicCamera camera) {
         this.mario = mario;
         this.camera = camera;
         this.batch = new SpriteBatch();
+        //this.assetManager = assetManager;
+        //this.skin = assetManager.get(SKIN);
 
-
+        textureAtlas = new TextureAtlas(Gdx.files.internal("assets/UI/uiskin.atlas"));
+        skin = new Skin(Gdx.files.internal("assets/UI/uiskin.json"), textureAtlas);
         viewport = new FitViewport(DesktopLauncher.width, DesktopLauncher.height, new OrthographicCamera());
+        viewport.apply();
+
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
         stage = new Stage(viewport, batch);
 
         background = new Texture("assets/Images/thomas.jpg");
         backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("assets/Sound/thomas.mp3"));
     }
 
-    private TextButton addButton(String text) {
+/*    private TextButton addButton(String text) {
         TextButton button = new TextButton(text, skin);
         table.add(button);
         table.row();
         return button;
-    }
+    }*/
 
     @Override
     public void show() {
         backgroundMusic.play();
         backgroundMusic.setVolume(0.01f); // sets volume to 0.1%
 
+        Gdx.input.setInputProcessor(stage);
+
         Table table = new Table();
         table.setFillParent(true);
         stage.addActor(table);
 
-        addButton( "play");
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton optionButton = new TextButton("Option", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        playButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("play was pressed");
+            }
+        });
+        optionButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("option was pressed");
+            }
+        });
+        exitButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("exit was pressed");
+            }
+        });
+
+        table.add(playButton);
+        table.row();
+        table.add(optionButton);
+        table.row();
+        table.add(exitButton);
+
+        stage.addActor(table);
     }
 
 
     @Override
     public void render(float v) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
         stage.draw();
-        /*stage.getBatch().begin();
-        stage.getBatch().draw(background, 0, 0, DesktopLauncher.width, DesktopLauncher.height);
-        stage.getBatch().end();*/
     }
 
     @Override
@@ -112,6 +134,7 @@ public class MainMenu implements Screen {
 
     @Override
     public void dispose() {
-
+        skin.dispose();
+        textureAtlas.dispose();
     }
 }
