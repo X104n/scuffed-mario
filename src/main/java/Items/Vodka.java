7c2dc5b7056229ffc9e95d71c2644fae914970fa
Sander.kavli <vas011@uib.no>
2022-04-27 21:37:38 +0200

    Added two item classes

---
+++ src/main/java/Items/Vodka.java
@@ -0,0 +1,11 @@
+package Items;
+
+import Objects.Entity;
+import Objects.Player;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.awt.*;
+
+public class Vodka extends Entity {
+
+}
